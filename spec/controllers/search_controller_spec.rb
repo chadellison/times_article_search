f9436f03c89fb62a4bd820909e4ecdb2312a01e0
Chad Ellison <chad.ellison0123@gmail.com>
2017-04-05 17:17:29 -0600

    add specs for search creation

--- spec/controllers/search_controller_spec.rb
+++ spec/controllers/search_controller_spec.rb
@@ -36,6 +36,32 @@ RSpec.describe SearchController, type: :controller do
         post :create, params: { search: { q: key_word } }
         expect(response.status).to eq 200
       end
+
+      context "when a search is on a new keyword" do
+        it "creates a new search object" do
+          allow(ArticleService).to receive(:search).with(key_word)
+            .and_return(api_data)
+
+          expect{ post :create, params: { search: { q: key_word } } }
+            .to change{ Search.count }.by(1)
+
+          expect(Search.last.count).to eq 1
+        end
+      end
+
+      context "when a search is on an old keyword" do
+        it "does not create a new search object; it increments the search object's count" do
+          search = Search.create(keyword: "word", count: 1)
+
+          allow(ArticleService).to receive(:search).with("word")
+            .and_return(api_data)
+
+          expect{ post :create, params: { search: { q: "word" } } }
+            .not_to change{ Search.count }
+
+          expect(search.reload.count).to eq 2
+        end
+      end
     end
 
     context "with an unsuccessful search" do
@@ -49,6 +75,15 @@ RSpec.describe SearchController, type: :controller do
         post :create, params: { search: { q: key_word } }
         expect(flash[:errors]).to eq "This is an error"
         expect(response.status).to eq 302
+      end
+
+      it "does not change the search count" do
+        allow(ArticleService).to receive(:search).with(key_word)
+          .and_return(error_data)
+
+        expect(Search).not_to receive(:find_or_create_by)
+        expect{ post :create, params: { search: { q: key_word } }}
+          .not_to change{ Search.count }
       end
     end
   end
